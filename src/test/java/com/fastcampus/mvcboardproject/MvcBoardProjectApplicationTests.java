f39209b095bc53257a6f40107d65234c4693229c
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2024-02-11 15:53:41 +0900

    #1 - 프로젝트 시작
    - Java + Spring Boot
    - Spring web : 웹 서비스 제공
    - Spring data JPA, H2 database, MySQL driver : 도메인 설계 및 DB 제공
    - Rest Repositories, Rest Repositories HAL Explorer : JSON API로 데이터 제공
    - Thymeleaf : 강의 목표에 맞춰 서버 사이드 렌더링 접근을 위한 템플릿 엔진
    - Bootstrap : 디자인 요소
    - Validation : 적절한 입출력 데이터 검증
    - Spring Security : 인증 기능
    - Lombok, Spring Boot Devtools, Spring Boot Actuator : 생산성

---
+++ src/test/java/com/fastcampus/mvcboardproject/MvcBoardProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.mvcboardproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcBoardProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
