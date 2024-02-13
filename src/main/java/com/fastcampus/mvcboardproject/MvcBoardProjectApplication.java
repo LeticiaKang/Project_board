382748776236543c86253fb3e0cfc9193f4aac04
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2024-02-13 19:12:21 +0900

    #16 - thymeleaf decoupled logic에 대한 사용자 정의 프로퍼티 설정 추가 및 적용
    - thymeleaf 프로퍼티 설정 중 decoupled logic이 없어 해당 프로퍼티를 추가하여 적용함.
    - decoupled logic를 권장하고 있음.

--- src/main/java/com/fastcampus/mvcboardproject/MvcBoardProjectApplication.java
+++ src/main/java/com/fastcampus/mvcboardproject/MvcBoardProjectApplication.java
@@ -2,7 +2,9 @@ package com.fastcampus.mvcboardproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan // 프로퍼티 클래스를 찾아서 빈으로 등록해주는 역할
 @SpringBootApplication
 public class MvcBoardProjectApplication {
 
