1e3778e35692e490f5cdaeba9b2ff68d6bce1535
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2024-02-17 00:08:33 +0900

    #27 - 게시판 컨트롤러 구현
    - 게시판 기본 조회 화면을 위한 컨트롤러 구현하고 데이터 전달 완료.
    - 게시판 페이지 기본 정렬 속성 부여
       : 10개 1페이지, 시간 내림차순

---
+++ src/main/java/com/fastcampus/mvcboardproject/dto/response/ArticleResponse.java
@@ -0,0 +1,40 @@
+package com.fastcampus.mvcboardproject.dto.response;
+
+import com.fastcampus.mvcboardproject.dto.ArticleDto;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public record ArticleResponse(
+        Long id,
+        String title,
+        String content,
+        String hashtag,
+        LocalDateTime createdAt,
+        String email,
+        String nickname
+) implements Serializable {
+
+    public static ArticleResponse of(Long id, String title, String content, String hashtag, LocalDateTime createdAt, String email, String nickname) {
+        return new ArticleResponse(id, title, content, hashtag, createdAt, email, nickname);
+    }
+
+    // 맵퍼 : 게시글 목록을 보여줄 때, 기본적으로 닉네임을 보여주고 없으면 아이디를 보여준다.
+    public static ArticleResponse from(ArticleDto dto) {
+        String nickname = dto.userAccountDto().nickname();
+        if (nickname == null || nickname.isBlank()) {
+            nickname = dto.userAccountDto().userId();
+        }
+
+        return new ArticleResponse(
+                dto.id(),
+                dto.title(),
+                dto.content(),
+                dto.hashtag(),
+                dto.createdAt(),
+                dto.userAccountDto().email(),
+                nickname
+        );
+    }
+
+}
