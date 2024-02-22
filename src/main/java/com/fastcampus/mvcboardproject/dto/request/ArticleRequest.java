4a21610ccfc53f4f947967c35c036ef425827b76
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2024-02-22 21:10:28 +0900

    #44 - 변경된 article을 받는 requestDto 작성
    - 사용자가 변경을 하고 DB에 반영하기 위해 정보를 보낼 때 사용

---
+++ src/main/java/com/fastcampus/mvcboardproject/dto/request/ArticleRequest.java
@@ -0,0 +1,25 @@
+package com.fastcampus.mvcboardproject.dto.request;
+
+import com.fastcampus.mvcboardproject.dto.ArticleDto;
+import com.fastcampus.mvcboardproject.dto.UserAccountDto;
+
+public record ArticleRequest(
+        String title,
+        String content,
+        String hashtag
+) {
+
+    public static ArticleRequest of(String title, String content, String hashtag) {
+        return new ArticleRequest(title, content, hashtag);
+    }
+
+    public ArticleDto toDto(UserAccountDto userAccountDto) {
+        return ArticleDto.of(
+                userAccountDto,
+                title,
+                content,
+                hashtag
+        );
+    }
+
+}
