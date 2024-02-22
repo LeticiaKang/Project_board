20db8a9c250c815b72147ae407dd786f024c7357
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2024-02-22 20:39:49 +0900

    #44 - 게시글 CRUD에 대한 테스트 코드 및 서비스 로직 작성
    - 게시글 CRUD를 위해 유저와 함께 정보를 받도록 변경함.

--- src/test/java/com/fastcampus/mvcboardproject/service/ArticleServiceTest.java
+++ src/test/java/com/fastcampus/mvcboardproject/service/ArticleServiceTest.java
@@ -2,11 +2,12 @@ package com.fastcampus.mvcboardproject.service;
 
 import com.fastcampus.mvcboardproject.domain.Article;
 import com.fastcampus.mvcboardproject.domain.UserAccount;
-import com.fastcampus.mvcboardproject.domain.type.SearchType;
+import com.fastcampus.mvcboardproject.domain.constant.SearchType;
 import com.fastcampus.mvcboardproject.dto.ArticleDto;
 import com.fastcampus.mvcboardproject.dto.UserAccountDto;
 import com.fastcampus.mvcboardproject.dto.ArticleWithCommentsDto;
 import com.fastcampus.mvcboardproject.repository.ArticleRepository;
+import com.fastcampus.mvcboardproject.repository.UserAccountRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,6 +17,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -33,6 +35,7 @@ class ArticleServiceTest {
 
     @InjectMocks private ArticleService sut;  // sut : subject under test 테스트 대상을 의미 / @InjectMocks : 목을 주입하는 대상
     @Mock private ArticleRepository articleRepository;
+    @Mock private UserAccountRepository userAccountRepository;
 
     @DisplayName("[검색어 없이 검색] 검색어 없이 게시글을 검색하면, 게시글 리스트 페이지를 반환한다.")
     @Test
@@ -107,7 +110,7 @@ class ArticleServiceTest {
         given(articleRepository.findById(articleId)).willReturn(Optional.of(article));
 
         // When
-        ArticleWithCommentsDto dto = sut.getArticle(articleId);
+        ArticleDto dto = sut.getArticle(articleId);
 
         // Then
         assertThat(dto)
@@ -115,6 +118,42 @@ class ArticleServiceTest {
                 .hasFieldOrPropertyWithValue("title", article.getTitle())
                 .hasFieldOrPropertyWithValue("content", article.getContent())
                 .hasFieldOrPropertyWithValue("hashtag", article.getHashtag());
+        then(articleRepository).should().findById(articleId);
+    }
+
+    @DisplayName("[게시글 ID 조회] 게시글 ID로 조회하면, 댓글 달긴 게시글을 반환한다.")
+    @Test
+    void givenArticleId_whenSearchingArticleWithComments_thenReturnsArticleWithComments() {
+        // Given
+        Long articleId = 1L;
+        Article article = createArticle();
+        given(articleRepository.findById(articleId)).willReturn(Optional.of(article));
+
+        // When
+        ArticleWithCommentsDto dto = sut.getArticleWithComments(articleId);
+
+        // Then
+        assertThat(dto)
+                .hasFieldOrPropertyWithValue("title", article.getTitle())
+                .hasFieldOrPropertyWithValue("content", article.getContent())
+                .hasFieldOrPropertyWithValue("hashtag", article.getHashtag());
+        then(articleRepository).should().findById(articleId);
+    }
+
+    @DisplayName("댓글 달린 게시글이 없으면, 예외를 던진다.")
+    @Test
+    void givenNonexistentArticleId_whenSearchingArticleWithComments_thenThrowsException() {
+        // Given
+        Long articleId = 0L;
+        given(articleRepository.findById(articleId)).willReturn(Optional.empty());
+
+        // When
+        Throwable t = catchThrowable(() -> sut.getArticleWithComments(articleId));
+
+        // Then
+        assertThat(t)
+                .isInstanceOf(EntityNotFoundException.class)
+                .hasMessage("게시글이 없습니다 - articleId: " + articleId);
         then(articleRepository).should().findById(articleId);
     }
 
@@ -143,7 +182,7 @@ class ArticleServiceTest {
         // When
         // catchThrowable 메서드는 주어진 람다 표현식을 실행하고, 해당 람다가 예외를 throw하는지 여부를 검증
         // 예외를 반환하고, 그렇지 않으면 null을 반환
-        Throwable t = catchThrowable(() -> sut.getArticle(articleId));
+        Throwable t = catchThrowable(() -> sut.getArticleWithComments(articleId));
 
         // Then
         assertThat(t)
@@ -155,14 +194,16 @@ class ArticleServiceTest {
     @DisplayName("[새 게시글 생성] 게시글 정보를 입력하면, 게시글을 생성한다")
     @Test
     void givenArticleInfo_whenSavingArticle_thenSavesArticle() {
-        // Given
-        ArticleDto dto = createArticleDto(); //createArticleDto("title", "content", "#java")
+        /// Given
+        ArticleDto dto = createArticleDto();
+        given(userAccountRepository.getReferenceById(dto.userAccountDto().userId())).willReturn(createUserAccount());
         given(articleRepository.save(any(Article.class))).willReturn(createArticle());
 
         // When
         sut.saveArticle(dto);
 
         // Then
+        then(userAccountRepository).should().getReferenceById(dto.userAccountDto().userId());
         then(articleRepository).should().save(any(Article.class));
     }
 
@@ -177,7 +218,7 @@ class ArticleServiceTest {
         given(articleRepository.getReferenceById(dto.id())).willReturn(article);
 
         // When
-        sut.updateArticle(dto);
+        sut.updateArticle(dto.id(), dto);
 
         // Then
         assertThat(article)
@@ -195,7 +236,7 @@ class ArticleServiceTest {
         given(articleRepository.getReferenceById(dto.id())).willThrow(EntityNotFoundException.class);
 
         // When
-        sut.updateArticle(dto);
+        sut.updateArticle(dto.id(), dto);
 
         // Then
         then(articleRepository).should().getReferenceById(dto.id());
@@ -230,12 +271,15 @@ class ArticleServiceTest {
     }
 
     private Article createArticle() {
-        return Article.of(
+        Article article = Article.of(
                 createUserAccount(),
                 "title",
                 "content",
                 "#java"
         );
+        ReflectionTestUtils.setField(article, "id", 1L);
+
+        return article;
     }
 
     private ArticleDto createArticleDto() {
@@ -243,7 +287,8 @@ class ArticleServiceTest {
     }
 
     private ArticleDto createArticleDto(String title, String content, String hashtag) {
-        return ArticleDto.of(1L,
+        return ArticleDto.of(
+                1L,
                 createUserAccountDto(),
                 title,
                 content,
