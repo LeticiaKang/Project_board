e5648de650c42926ecc3560fef1b4d5d043edfda
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2024-03-03 04:17:03 +0900

    #61 - 회원가입 추가에 대한 Config 및 Controller 변경
    - 미사용 import를 제거하고, 디버깅 로그를 추가했다.
    
    - securityConfig에 Post method에 대한 접근 허용을 추가했다.
    - MainController에 회원가입 후 리다이렉션되는 페이지를 로그인에서 메인 페이지로 바꿨다.
    - JpaConfig는 회원가입시 미인증된 유저이므로 오류가 나지 않게 defaultUser를 넣게 했다.

--- src/main/java/com/fastcampus/mvcboardproject/config/JpaConfig.java
+++ src/main/java/com/fastcampus/mvcboardproject/config/JpaConfig.java
@@ -1,6 +1,7 @@
 package com.fastcampus.mvcboardproject.config;
 
 import com.fastcampus.mvcboardproject.dto.security.BoardPrincipal;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
@@ -11,18 +12,24 @@ import org.springframework.security.core.context.SecurityContext;
 
 import java.util.Optional;
 
-@EnableJpaAuditing
+@Slf4j
+@EnableJpaAuditing  // audit활성화
 @Configuration
 public class JpaConfig {
 
     @Bean
-    public AuditorAware<String> auditorAware(){
+    public AuditorAware<String> auditorAware() {
         return () -> Optional.ofNullable(SecurityContextHolder.getContext())
                 .map(SecurityContext::getAuthentication)
                 .filter(Authentication::isAuthenticated)  // 로그인한 상태인지 확인
                 .map(Authentication::getPrincipal)  // getPrincipal : return Object
-                .map(BoardPrincipal.class::cast)    // Type Casting
-                .map(BoardPrincipal::getUsername);
+//                .map(BoardPrincipal.class::cast)    // Type Casting
+//                .map(BoardPrincipal::getUsername)
+                .map(principal -> {
+                    if (principal instanceof BoardPrincipal) {
+                        return ((BoardPrincipal) principal).getUsername();
+                    }
+                    return "defaultUser";
+                });
     }
-
 }
--- src/main/java/com/fastcampus/mvcboardproject/config/SecurityConfig.java
+++ src/main/java/com/fastcampus/mvcboardproject/config/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.fastcampus.mvcboardproject.config;
 import com.fastcampus.mvcboardproject.dto.UserAccountDto;
 import com.fastcampus.mvcboardproject.dto.security.BoardPrincipal;
 import com.fastcampus.mvcboardproject.repository.UserAccountRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -15,6 +17,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Slf4j
 @Configuration
 public class SecurityConfig {
 
@@ -23,44 +26,48 @@ public class SecurityConfig {
      */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.debug("[로그][SecurityConfig][securityFilterChain] 인증 확인");
+
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((auth) -> auth
-                    .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                    .requestMatchers(HttpMethod.GET,
-                            "/",
-                            "/articles",
-                            "/articles/search-hashtag",
-                            "/sign-up").permitAll()
-                    .anyRequest().authenticated()
-            )
-            .formLogin(
-                    form -> form
-                            .loginPage("/login")
-                            .loginProcessingUrl("/login")
-                            .usernameParameter("username") // 로그인 폼의 사용자 이름 필드 이름
-                            .passwordParameter("password") // 로그인 폼의 비밀번호 필드 이름
-                            .defaultSuccessUrl("/")
-                            .permitAll()
-            )
-            .logout(logout -> logout
-                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃을 처리할 경로 지정
-                    .logoutSuccessUrl("/") // 로그아웃 성공 후 리디렉션될 경로 지정
-                    .invalidateHttpSession(true) // 로그아웃 시 HTTP 세션을 무효화
-                    .deleteCookies("JSESSIONID") // 로그아웃 시 쿠키 삭제
-                    .permitAll()
-            );
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers(HttpMethod.GET,
+                                "/",
+                                "/articles",
+                                "/articles/search-hashtag",
+                                "/sign-up").permitAll()
+                        .requestMatchers(HttpMethod.POST,
+                                "/sign-up/save",
+                                "/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .formLogin(
+                        form -> form
+                                .loginPage("/login")
+                                .loginProcessingUrl("/login")
+                                .defaultSuccessUrl("/")
+                                .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout")) // 로그아웃을 처리할 경로 지정
+                        .logoutSuccessUrl("/") // 로그아웃 성공 후 리디렉션될 경로 지정
+                        .invalidateHttpSession(true) // 로그아웃 시 HTTP 세션을 무효화
+                        .deleteCookies("JSESSIONID") // 로그아웃 시 쿠키 삭제
+                        .permitAll()
+                );
         return http.build();
     }
 
     /*
-        loadUserByUsername을 람다식으로 구현하였으면,
-        입력값은 userAccountRepository이고
-        반환값은 BoardPrincipal이다.
-        userName을 찾을 수 없는 경우, NotFoundException 예외가 발생한다.
-     */
+    loadUserByUsername을 람다식으로 구현하였으면,
+    입력값은 userAccountRepository이고
+    반환값은 BoardPrincipal이다.
+    userName을 찾을 수 없는 경우, NotFoundException 예외가 발생한다.
+    */
     @Bean
     public UserDetailsService userDetailsService(UserAccountRepository userAccountRepository) {
+        log.debug("[로그][SecurityConfig][userDetailsService] 인증 확인 : {}", userAccountRepository);
         return username ->
                 userAccountRepository.findById(username)            // User 객체를 가여좀
                         .map(UserAccountDto::from)     // User 객체를 UserAccountDto로 변환함(작성, 수정 정보는 null)
@@ -69,10 +76,11 @@ public class SecurityConfig {
     }
 
     /*
-        spring security의 인증기능을 구현할 때, 비밀번호 암호화를 위해 반드시 설정해야 한다.
-     */
+         spring security의 인증기능을 구현할 때, 비밀번호 암호화를 위해 반드시 설정해야 한다.
+      */
     @Bean
     public PasswordEncoder passwordEncoder() {
+        log.debug("[로그][SecurityConfig][PasswordEncoder] 비밀번호 암호화");
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
