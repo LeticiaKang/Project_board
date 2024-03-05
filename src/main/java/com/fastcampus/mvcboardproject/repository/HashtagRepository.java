dcce643f9d371d9dc23256fa24468ef40ddbf9bf
Leticia.k <87592790+LeticiaKang@users.noreply.github.com>
2024-03-05 11:39:50 +0900

    #57 - 해시태그 repository 추가

---
+++ src/main/java/com/fastcampus/mvcboardproject/repository/HashtagRepository.java
@@ -0,0 +1,22 @@
+package com.fastcampus.mvcboardproject.repository;
+
+import com.fastcampus.mvcboardproject.domain.Hashtag;
+import com.fastcampus.mvcboardproject.repository.querydsl.HashtagRepositoryCustom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+@RepositoryRestResource
+public interface HashtagRepository extends
+        JpaRepository<Hashtag, Long>,
+        HashtagRepositoryCustom,
+        QuerydslPredicateExecutor<Hashtag> {
+
+    Optional<Hashtag> findByHashtagName(String hashtagName);
+    List<Hashtag> findByHashtagNameIn(Set<String> hashtagNames);
+
+}
